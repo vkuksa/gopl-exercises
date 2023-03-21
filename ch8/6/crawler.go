865b6396c11c4e5fd980776a22f8d15852ad1fd8
vkuksa <kuksa.vladyslav@gmail.com>
2023-03-21 14:50:58 +0200

    Added 8.6

---
+++ ch8/6/crawler.go
@@ -0,0 +1,73 @@
+// Exercise 8.6: Add depth-limiting to the concurrent crawler. That is, if the user sets -depth=3,
+// then only URLs reachable by at most three links will be fetched.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"gopl-exercises/ch5/links"
+	"log"
+	"math"
+	"os"
+)
+
+type Link struct {
+	url   string
+	depth int
+}
+type Links struct {
+	urls  []string
+	depth int
+}
+
+func crawl(url string) (results []string) {
+	fmt.Println(url)
+	results, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return
+}
+
+var maxDepth = flag.Int("depth", math.MaxInt, "depth of crawl limit")
+
+func init() {
+	flag.Parse()
+}
+
+// !+
+func main() {
+	worklist := make(chan Links)   // lists of URLs, may have duplicates
+	unseenLinks := make(chan Link) // de-duplicated URLs
+
+	// Init list with command line arguments
+	go func() { worklist <- Links{os.Args[1:], 0} }()
+
+	// Create 20 crawler goroutines to fetch each unseen link.
+	for i := 0; i < 20; i++ {
+		go func() {
+			for link := range unseenLinks {
+				foundLinks := crawl(link.url)
+				go func(depth int) { worklist <- Links{foundLinks, depth} }(link.depth + 1)
+			}
+		}()
+	}
+
+	// The main goroutine de-duplicates worklist items
+	// and sends the unseen ones to the crawlers.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		if list.depth <= *maxDepth {
+			for _, link := range list.urls {
+				if !seen[link] {
+					seen[link] = true
+					unseenLinks <- Link{link, list.depth}
+				}
+			}
+		}
+	}
+}
+
+//!-
